ef17592765c66622fd6c185e46f945210b7efad3
zoro98555-stack <zoro98555@gmail.com>
2026-07-17 15:24:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.querySelector("button").addEventListener("click", function () {
     window.location.href = "historia.html";
 });
+const inicio = new Date("2026-05-06T00:00:00");
+
+function atualizarContador() {
+    const agora = new Date();
+
+    const diferenca = agora - inicio;
+
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
+    document.getElementById("contador").innerHTML =
+        "❤️ Estamos juntos há<br><br>" + dias + " dias";
+}
+
+atualizarContador();
+setInterval(atualizarContador, 1000);
