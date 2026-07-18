021b54a4f52f1dbbadce96e6d3ff1b88cae6d132
zoro98555-stack <zoro98555@gmail.com>
2026-07-18 04:23:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function atualizarContador() {
     const contador = document.getElementById("contador");
 
     if (contador) {
+
         const agora = new Date();
 
         const diferenca = agora - inicio;
@@ -37,8 +38,10 @@ function atualizarContador() {
         const minutos = Math.floor(diferenca / (1000 * 60)) % 60;
         const horas = Math.floor(diferenca / (1000 * 60 * 60)) % 24;
         const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
         const meses = Math.floor(dias / 30);
         const diasRestantes = dias % 30;
+
 
         contador.innerHTML = `
         ❤️ Estamos juntos há ❤️<br><br>
@@ -56,14 +59,38 @@ atualizarContador();
 setInterval(atualizarContador, 1000);
 
 
+
 function tocarMusica() {
+
     const musica = document.getElementById("musica");
 
     if (musica) {
+
         if (musica.paused) {
             musica.play();
         } else {
             musica.pause();
         }
+
     }
+
+}
+
+
+// Abrir e fechar carta 💌
+
+function abrirCarta() {
+
+    const carta = document.getElementById("carta");
+
+    if (carta) {
+
+        if (carta.style.display === "none") {
+            carta.style.display = "block";
+        } else {
+            carta.style.display = "none";
+        }
+
+    }
+
 }
