ff6a84bc3db916bcba4ac918e59d74336e1b5d7a
zoro98555-stack <zoro98555@gmail.com>
2026-07-18 03:24:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,49 +2,68 @@ const texto = "João Gabriel ❤️ Luana";
 let indice = 0;
 
 function escreverNome() {
-    if (indice < texto.length) {
-        document.getElementById("titulo").innerHTML += texto.charAt(indice);
+    const titulo = document.getElementById("titulo");
+
+    if (titulo && indice < texto.length) {
+        titulo.innerHTML += texto.charAt(indice);
         indice++;
         setTimeout(escreverNome, 150);
     }
 }
 
 escreverNome();
-document.querySelector("button").addEventListener("click", function () {
-    window.location.href = "historia.html";
-});
+
+
+const botaoHistoria = document.getElementById("botaoHistoria");
+
+if (botaoHistoria) {
+    botaoHistoria.addEventListener("click", function () {
+        window.location.href = "historia.html";
+    });
+}
+
+
 const inicio = new Date("2026-05-06T00:00:00");
 
 function atualizarContador() {
-    const agora = new Date();
+    const contador = document.getElementById("contador");
 
-    const diferenca = agora - inicio;
+    if (contador) {
+        const agora = new Date();
 
-    const segundos = Math.floor(diferenca / 1000) % 60;
-const minutos = Math.floor(diferenca / (1000 * 60)) % 60;
-const horas = Math.floor(diferenca / (1000 * 60 * 60)) % 24;
-const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-const meses = Math.floor(dias / 30);
-const diasRestantes = dias % 30;
-document.getElementById("contador").innerHTML = `
-❤️ Estamos juntos há ❤️<br><br>
+        const diferenca = agora - inicio;
 
-${meses} meses<br>
-${diasRestantes} dias<br>
-${horas} horas<br>
-${minutos} minutos<br>
-${segundos} segundos
-`;
+        const segundos = Math.floor(diferenca / 1000) % 60;
+        const minutos = Math.floor(diferenca / (1000 * 60)) % 60;
+        const horas = Math.floor(diferenca / (1000 * 60 * 60)) % 24;
+        const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+        const meses = Math.floor(dias / 30);
+        const diasRestantes = dias % 30;
+
+        contador.innerHTML = `
+        ❤️ Estamos juntos há ❤️<br><br>
+
+        ${meses} meses<br>
+        ${diasRestantes} dias<br>
+        ${horas} horas<br>
+        ${minutos} minutos<br>
+        ${segundos} segundos
+        `;
+    }
 }
 
 atualizarContador();
 setInterval(atualizarContador, 1000);
-function tocarMusica(){
+
+
+function tocarMusica() {
     const musica = document.getElementById("musica");
 
-    if(musica.paused){
-        musica.play();
-    } else {
-        musica.pause();
+    if (musica) {
+        if (musica.paused) {
+            musica.play();
+        } else {
+            musica.pause();
+        }
     }
 }
